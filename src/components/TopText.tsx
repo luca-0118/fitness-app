e80cc4a777fa365293547e9086ae9cc9413c3e3e
luca-0118 <luca-0118@hotmail.com>
2026-02-25 23:48:07 +0100

    ¯\_(ツ)_/¯

---
+++ src/components/TopText.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function TopText() {
+    return (
+        <header className="text-[24px] font-bold text-[#F2F3F2]">
+            Workout
+        </header>
+    
+    )
+}
+// ¯\_(ツ)_/¯
