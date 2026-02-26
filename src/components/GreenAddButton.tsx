89bac44e4ee4be45ef998ec5e2de47b97033bdd5
Lars <lars.westenberg@student.hu.nl>
2026-02-26 12:16:40 +0100

    :) :) :)

--- src/components/GreenAddButton.tsx
+++ src/components/GreenAddButton.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
 
 export default function GreenAddButton() {
     const navigate = useNavigate();
@@ -6,9 +7,7 @@ export default function GreenAddButton() {
     return (
     <button onClick={() => navigate("/new-workout")}
             className="fixed bottom-30 right-6 h-16 w-16 rounded-full bg-[#40C057] hover:bg-[#5AEA74] flex items-center justify-center z-50">
-        <svg xmlns="http://www.w3.org/2000/svg" height="33px" viewBox="0 -960 960 960" width="33px" fill="#FFFFFF" className="">
-            <path d="M446.67-120v-326.67H120v-66.66h326.67V-840h66.66v326.67H840v66.66H513.33V-120h-66.66Z"/>
-        </svg>
+            <AddIcon sx={{ fontSize: 49 }}/>
     </button>
     )
 }
