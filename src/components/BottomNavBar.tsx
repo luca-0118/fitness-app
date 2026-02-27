8bd634bff853ea1b6e97fc1934ddc4309c3dc661
Mukdembu <Mukdembuwan@gmail.com>
2026-02-27 03:33:47 +0100

    de pagina waar je op zit wordt groen in de nav bar

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import RestaurantSharpIcon from '@mui/icons-material/RestaurantSharp';
@@ -9,23 +8,25 @@ export default function BottomNavBar() {
     return (
         <footer className="fixed bottom-0 z-10 h-22 w-full bg-[#333737] border-t-2 border-[#565d5d]">
             <nav className="grid grid-cols-4 h-full max-w-sm mx-auto">
-                <Link to="/" className="inline-flex flex-col items-center">
+                <NavLink to="/" className={({ isActive }) => `inline-flex flex-col items-center ${isActive ? 'text-[#40C057]' : 'text-[#ffffff]'}`}>
                     <HomeSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Home</p>
-                </Link>
-                <Link to="/workouts" className="inline-flex flex-col items-center">
+                </NavLink>
+                <NavLink to="/workouts" className={({ isActive }) => `inline-flex flex-col items-center ${isActive ? 'text-[#40C057]' : 'text-[#ffffff]'}`}>
                     <FitnessCenterIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Workout</p>
-                </Link>
-                <Link to="/kcal-tracker" className="inline-flex flex-col items-center">
+                </NavLink>
+                <NavLink to="/kcal-tracker" className={({ isActive }) => `inline-flex flex-col items-center ${isActive ? 'text-[#40C057]' : 'text-[#ffffff]'}`}>
                     <RestaurantSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Kcal</p>
-                </Link>
-                <Link to="/profile" className="inline-flex flex-col items-center">
+                </NavLink>
+                <NavLink to="/profile" className={({ isActive }) => `inline-flex flex-col items-center ${isActive ? 'text-[#40C057]' : 'text-[#ffffff]'}`}>
                     <PersonSharpIcon sx={{ fontSize: 40 }}/>
                     <p className="text-xs">Profile</p>
-                </Link>
+                </NavLink>
             </nav>
         </footer>
     )
 }
+
+//code heeft veel repetitie (basically zelfde code), ik ga kijken als ik dit later netter kan maken. maar voor nu hebben we iets
