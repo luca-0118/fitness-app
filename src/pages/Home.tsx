fc75f56b16fc31f6ff1a44991b56e430b8869f1d
Lars <lars.westenberg@student.hu.nl>
2026-03-03 09:21:25 +0100

    RadialBar chart instead of donut chart. Delete The CaloriesDonutChart.tsx Altered the routing in Home.tsx again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import CaloriesDonutChart from "../components/CaloriesDonutChart";
+import {NutritionDonutChart} from "../components/NutritionDonutChart";
 import WeightLineChart from "../components/WeightLineChart.tsx";
-import WeeklyCaloriesChart from "../components/WeeklyCaloriesChart.tsx"; // Make sure the path is correct
+import WeeklyCaloriesChart from "../components/WeeklyCaloriesChart.tsx";
 
 export default function Home() {
     const navigate = useNavigate();
@@ -10,31 +10,24 @@ export default function Home() {
         <div className="flex w-screen pb-24">
             <div className="grid grid-cols-2 gap-4 py-4 w-[90%] mx-auto">
                 <div className="col-span-2 bg-[#1E1E1E] border border-[#414141] rounded-xl p-6 font-bold flex flex-col items-center">
-                    <h2 className="border-b-2 border-white w-full text-center mb-4">Calorie intake</h2>
-                    <CaloriesDonutChart />
+                    <h2 className="border-b-2 border-white w-full text-center mb-4 text-lg">Nutrition charts</h2>
+                        <NutritionDonutChart />
                 </div>
 
                 <button
-                    onClick={() => navigate("/workouts")}
-                    className="bg-[#1E1E1E] border border-[#414141] hover:bg-[#252525] rounded-xl p-6 font-bold cursor-pointer"
-                >
-                    Workout
-                </button>
-
-                <button
                     onClick={() => navigate("/history")}
-                    className="bg-[#1E1E1E] border border-[#414141] hover:bg-[#252525] rounded-xl p-6 font-bold cursor-pointer"
+                    className="bg-[#1E1E1E] border border-[#414141] hover:bg-[#252525] rounded-xl p-6 font-bold cursor-pointer col-span-2"
                 >
                     Workout history
                 </button>
 
-                <div className="bg-[#1E1E1E] border border-[#414141] rounded-xl p-6 font-bold text-[14px] col-span-2 items-center">
-                    <h2 className="border-b-2 border-white w-full text-center mb-4">Weight</h2>
+                <div className="bg-[#1E1E1E] border border-[#414141] rounded-xl p-6 col-span-2 items-center">
+                    <h2 className="border-b-2 border-white w-full text-center mb-4 font-bold text-lg">Weight</h2>
                     <WeightLineChart />
                 </div>
 
-                <div className="bg-[#1E1E1E] border border-[#414141] rounded-xl p-6 font-bold text-[14px] col-span-2 items-center">
-                    <h2 className="border-b-2 border-white text-center mb-4">Weekly calorie intake</h2>
+                <div className="bg-[#1E1E1E] border border-[#414141] rounded-xl p-6 col-span-2 items-center">
+                    <h2 className="border-b-2 border-white text-center mb-4 font-bold text-lg">Weekly calorie intake</h2>
                     <WeeklyCaloriesChart />
                 </div>
             </div>
