674a844c4965ca8245fa1d73df2f39de46083c92
Lars <lars.westenberg@student.hu.nl>
2026-03-04 11:19:45 +0100

    import Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import BottomNavBar from "./components/BottomNavBar.tsx";
 
 import Home from "./pages/Home.tsx";
