1c800f596d2ccaadf2e93929a7863e58818f4eb6
Lars Konermann <144104477+larskonermann@users.noreply.github.com>
2026-03-17 11:13:36 +0100

    - removed draggable component in overview, should only be posible in edit mode (#22)
    - added gifs to exercise overview

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,66 +1,67 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 type backendFunctions =
-    | "create_workout"
-    | "list_workouts"
-    | "link_exercise"
-    | "create_exercise"
-    | "get_all_exercises"
-    | "get_workout"
-    | "start_session"
-    | "get_session";
+  | "create_workout"
+  | "list_workouts"
+  | "link_exercise"
+  | "create_exercise"
+  | "get_all_exercises"
+  | "get_workout"
+  | "start_session"
+  | "get_session";
 
 interface WorkoutDTO {
-    uuid: string;
-    name: string;
-    desc?: string;
+  uuid: string;
+  name: string;
+  desc?: string;
 }
 
 interface ExerciseDTO {
-    instanceId: number;
-    id: string;
-    name: string;
-    data: string;
+  instanceId: number;
+  id: string;
+  name: string;
+  data: string;
+  gif_url: string;
 }
 
 interface linkExerciseDTO {
-    workout_uuid: string;
-    exercise_uuid: string;
+  workout_uuid: string;
+  exercise_uuid: string;
 }
 
 interface ApiError extends ApiReponse {
-    ok: false;
-    error_type: string;
-    message: string;
+  ok: false;
+  error_type: string;
+  message: string;
 }
 
 interface ApiReponse {
-    ok: boolean;
+  ok: boolean;
 }
 
 interface ApiSucess<T> extends ApiReponse {
-    ok: true;
-    data: T;
+  ok: true;
+  data: T;
 }
 
 interface IdetailedWorkoutDTO {
-    uuid: string;
-    name: string;
-    desc: string;
-    exercises: ExerciseDTO[];
+  uuid: string;
+  name: string;
+  desc: string;
+  exercises: ExerciseDTO[];
 }
 
 interface ISessionState {
-    workout_name: String,
-    session_uuid: String,
-    start_time: String,
-    end_time: String,
-    exercises: SessionExercises[]
+  workout_name: String;
+  session_uuid: String;
+  start_time: String;
+  end_time: String;
+  exercises: SessionExercises[];
 }
 
 interface SessionExercises {
-    exercise_id: String,
-    name: String,
-    reps: Number,
-    weight: Number,
-    time_completed: String
+  exercise_id: String;
+  name: String;
+  reps: Number;
+  weight: Number;
+  time_completed: String;
 }
