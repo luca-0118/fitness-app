9b6354c45145b6d831ac7371e75ec9adf83ccd4e
Lars Konermann <144104477+larskonermann@users.noreply.github.com>
2026-03-17 11:13:36 +0100

    - removed draggable component in overview, should only be posible in edit mode (#22)
    - added gifs to exercise overview
    
    (cherry picked from commit 1c800f596d2ccaadf2e93929a7863e58818f4eb6)

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -5,63 +5,72 @@ import { move } from "@dnd-kit/helpers";
 import StartSessionButton from "../components/StartSessionButton.tsx";
 import API from "../classes/api.ts";
 import { useWorkout } from "../context/WorkoutContext.tsx";
-import {PointerSensor, PointerActivationConstraints, DragDropManager} from '@dnd-kit/dom';
+import {
+  PointerSensor,
+  PointerActivationConstraints,
+  DragDropManager,
+} from "@dnd-kit/dom";
 const manager = new DragDropManager({
-    sensors: [
-        PointerSensor.configure({
-            activationConstraints: [
-                new PointerActivationConstraints.Delay({
-                    value: 150,
-                    tolerance: {x: 5, y: 5},
-                }),
-            ]
-        })
-    ]
+  sensors: [
+    PointerSensor.configure({
+      activationConstraints: [
+        new PointerActivationConstraints.Delay({
+          value: 150,
+          tolerance: { x: 5, y: 5 },
+        }),
+      ],
+    }),
+  ],
 });
 export default function Exercises() {
-    /* muk data, moet uiteindelijk een GET API worden*/
-    const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
-    const { selectedWorkout } = useWorkout();
+  /* muk data, moet uiteindelijk een GET API worden*/
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
+  const { selectedWorkout } = useWorkout();
 
-    useEffect(() => {
-        const getData = async () => {
-            
-            const hi = await API.workouts.detailed(selectedWorkout);
-            if (typeof hi === "string") {
-                return;
-            }
-            setExercises(
-            hi.exercises.map((exercise: ExerciseDTO, index: number) => ({
-                ...exercise,
-                instanceId: index
-            })));
-        };
-        getData();
-    }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const hi = await API.workouts.detailed(selectedWorkout);
+      if (typeof hi === "string") {
+        return;
+      }
+      setExercises(
+        hi.exercises.map((exercise: ExerciseDTO, index: number) => ({
+          ...exercise,
+          instanceId: index,
+        })),
+      );
+    };
+    getData();
+  }, []);
 
-    return (
-        <>
-            <div className="pb-18 pt-2">
-                <DragDropProvider manager={manager}
-                    onDragEnd={(event) => {
-                        setExercises((exercises) => move(exercises, event));
-                    }}
-                >
-                    <ul>
-                        {exercises.map((exercise, index) => (
-                            <ExerciseOverviewWidget
-                                key={exercise.instanceId}
-                                id={exercise.instanceId.toString()}
-                                index={index}
-                                name={exercise.name}
-                            />
-                        ))}
-                    </ul>
-                </DragDropProvider>
-                <div className="absolute bottom-0 pb-24 w-full opacity-80">
-                    <StartSessionButton exercises={exercises} workoutId={selectedWorkout} />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="pb-18 pt-2">
+        <DragDropProvider
+          manager={manager}
+          onDragEnd={(event) => {
+            setExercises((exercises) => move(exercises, event));
+          }}
+        >
+          <ul>
+            {exercises.map((exercise, index) => (
+              <ExerciseOverviewWidget
+                key={exercise.instanceId}
+                id={exercise.instanceId.toString()}
+                index={index}
+                name={exercise.name}
+                gif={exercise.gif_url}
+              />
+            ))}
+          </ul>
+        </DragDropProvider>
+        <div className="absolute bottom-0 pb-24 w-full opacity-80">
+          <StartSessionButton
+            exercises={exercises}
+            workoutId={selectedWorkout}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
