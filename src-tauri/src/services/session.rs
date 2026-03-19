ab5b60f733f2b5d37f80a0f254b6b35f7dd2042f
Zico <zicobacker26@gmail.com>
2026-03-19 09:53:53 +0100

    clear session logic
    - new function that clears session.

--- src-tauri/src/services/session.rs
+++ src-tauri/src/services/session.rs
@@ -1,12 +1,25 @@
 use std::sync::Mutex;
 use crate::SessionState;
 
-pub fn start(session: &SessionState, session_state: tauri::State<Mutex<SessionState>>) -> bool {
+pub fn start(session: &SessionState, session_state: &tauri::State<Mutex<SessionState>>) -> bool {
    let mut session_state = session_state.lock().unwrap();
     *session_state = session.clone();
-    return true;
+    
+    true
 }
-pub fn get(session_state: tauri::State<Mutex<SessionState>>) -> SessionState {
+pub fn get(session_state: &tauri::State<Mutex<SessionState>>) -> SessionState {
     let session_state = session_state.lock().unwrap();
     session_state.clone()
+}
+pub fn clear(session_state: &tauri::State<Mutex<SessionState>>) -> bool {
+    let mut session_state = session_state.lock().unwrap();
+
+    // TODO find alternative to do this.
+    session_state.session_uuid = String::new();
+    session_state.workout_name = String::new();
+    session_state.start_time = String::new();
+    session_state.end_time = String::new();
+    session_state.exercises = Vec::new();
+
+    true
 }
