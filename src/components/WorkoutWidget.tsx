7ed83a4f945ca190905c30976b5c460de1f0604d
Mukdembu <107514335+Mukdembu@users.noreply.github.com>
2026-03-20 09:28:49 +0100

    Dropdown menu for workouts. (Edit / Delete) (#34)

--- src/components/WorkoutWidget.tsx
+++ src/components/WorkoutWidget.tsx
@@ -1,7 +1,9 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import { useSortable } from "@dnd-kit/react/sortable";
 import { useWorkout } from "../context/WorkoutContext";
 
@@ -13,10 +15,27 @@ interface WorkoutWidgetProps {
 
 export default function WorkoutWidget({ id, index, name }: WorkoutWidgetProps) {
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
     const [element, setElement] = useState<HTMLElement | null>(null);
     const handleRef = useRef<HTMLButtonElement | null>(null);
     const { isDragging } = useSortable({ id, index, element, handle: handleRef });
     const { setSelectedWorkout } = useWorkout();
+
+    const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        function handleMenuClose(event: MouseEvent) {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target as Node)
+            ) {
+                setOpen(false);
+            }
+        }
+
+        document.addEventListener("pointerdown", handleMenuClose);
+        return () => document.removeEventListener("pointerdown", handleMenuClose);
+    }, []);
 
     const handleNavigation = () => {
         setSelectedWorkout(id);
@@ -37,9 +56,18 @@ export default function WorkoutWidget({ id, index, name }: WorkoutWidgetProps) {
                 <h2 className="text-lg font-semibold">{name}</h2>
             </button>
 
-            <button className="ml-auto cursor-pointer" onClick={() => navigate("/edit-workout")}>
+            <div ref={dropdownRef} className="ml-auto cursor-pointer relative" onClick={() => setOpen((prev) => !prev)}>
                 <MoreVertIcon sx={{ fontSize: 40 }} />
-            </button>
+                <div className={`absolute z-100 top-full right-1 mt-1 flex flex-col rounded-xl p-2 bg-[#1E1E1E] border border-gray-600 transform transition-all duration-100 ease-out origin-top-right 
+                ${open ? "opacity-100 scale-100" : "opacity-0 scale-95 -translate-y-2 pointer-events-none"}`}>
+                    <button className="w-full hover:bg-gray-700 flex items-center gap-2 px-3 py-2 rounded-xl" onClick={() => {setOpen(false); navigate("/edit-workout");}}>
+                        <EditIcon className="w-5 h-5" /> Edit
+                    </button>
+                    <button className="w-full hover:bg-gray-700 text-red-500 flex items-center gap-2 px-3 py-2 rounded-xl" onClick={() => {setOpen(false);} /*TODO add delete functionality*/ }>
+                        <DeleteIcon className="w-5 h-5" /> Delete
+                    </button>
+                </div>
+            </div>
         </li>
     );
 }
