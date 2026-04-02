9af63e9e5e81cdc9ff418930d185c2938f2df702
Zicohh <zicobacker26@gmail.com>
2026-04-02 15:41:26 +0200

    open & close selected workouts

--- src/components/SelectedExercisesModal.tsx
+++ src/components/SelectedExercisesModal.tsx
@@ -3,6 +3,7 @@ import { Iworkout } from "../context/WorkoutContext"
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { useNavigate } from "react-router-dom";
 import { ExerciseAction, ExercisesActionKind, ExerciseState } from "../Hooks/reducers/exerciseSelectReducer";
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 type SelectedExerciseModalProps = {
     dispatch: ActionDispatch<[action: ExerciseAction]>;
@@ -10,10 +11,9 @@ type SelectedExerciseModalProps = {
     saveFunc: () => void;
 }
 export default function SelectedExerciseModal({dispatch,state, saveFunc}:SelectedExerciseModalProps) {
-
     const [exerciseRows,setExerciseRows] = useState<ReactNode[]>([]);
+    const [open,isOpen] = useState<boolean>(true);
     const navigate = useNavigate();
-
     const exerciseCount = state.exercises.length;
 
     useEffect(() =>{
@@ -21,15 +21,15 @@ export default function SelectedExerciseModal({dispatch,state, saveFunc}:Selecte
             <ExerciseRow exercise={exercise} 
                          dispatcher={dispatch}
             />);
-
         setExerciseRows(rows);
     },[state.exercises]);
 
     return <div className="absolute z-100 bottom-20 left-[2dvw] w-[96dvw]">
-        <ul className={`px-2 pt-2 mx-4 bg-[#F17F41] flex gap-2 flex-col rounded-t-2xl overflow-scroll max-h-30 ${exerciseRows.length == 0 ? "hidden":""}`}>
+        <ul className={`px-2 pt-2 mx-4 bg-[#F17F41] flex gap-2 flex-col rounded-t-2xl overflow-y-scroll max-h-30 ${exerciseRows.length == 0 || !open ? "hidden":""}`}>
             {exerciseRows}
         </ul>
-        <div className="bg-[#F67631] flex justify-between px-8 rounded-xl items-center border border-[#DC6728]">
+        <div className="bg-[#F67631] flex justify-between pr-8 pl-2 rounded-xl items-center border border-[#DC6728]">
+            <ListOpenButton open={open} onOpen={(opened) => {isOpen(opened)}}/>
             <p className="text-2xl">{exerciseCount} exercise{exerciseCount == 1 ? "" : "s"} added</p>
             <button className="text-4xl" onClick={() => {
                 saveFunc();
@@ -40,11 +40,37 @@ export default function SelectedExerciseModal({dispatch,state, saveFunc}:Selecte
 }
 
 
+type ListOpenButtonProps = {
+    open: boolean;
+    onOpen: (val: boolean) => void
+}
+/**
+ * Inline component to handle opening and closing the modal with the list of exercises.
+ * @param open - a true false boolean required to tell us if the modal is open or not
+ * @param onOpen - wrapper of the isOpen functionality.
+ * @constructor
+ */
+function ListOpenButton({open,onOpen}:ListOpenButtonProps): ReactNode {
+    const handleOpen = () => {
+        onOpen(!open);
+    }
+
+    return <div onClick={handleOpen}>
+        {open ? <ArrowDropUpIcon  style={{rotate: "180deg"}}/> : <ArrowDropUpIcon/>}
+    </div>
+}
+
+
 type exerciseRowProps = {
     exercise: Iworkout;
     dispatcher: ActionDispatch<[action: ExerciseAction]>
 }
-
+/**
+ * Seperate in-file component for the rows to keep the code a bit more structured in the for loop.
+ * @param exercise - the details of the exercise
+ * @param dispatcher - dispatcher object from our Reducer
+ * @constructor
+ */
 function ExerciseRow({exercise,dispatcher}: exerciseRowProps): ReactNode {
     return <>
     <li className="flex items-center justify-between" key={exercise.id}>
