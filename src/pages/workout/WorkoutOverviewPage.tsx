30dda715a9fd4c30db0096c623517bdba5aeeb1f
Zico <zicobacker26@gmail.com>
2026-05-06 10:27:17 +0200

    [FIX] Workout exercise create button styling issue. - caused by height changing inside button, now added padding instead.

--- src/pages/workout/WorkoutOverviewPage.tsx
+++ src/pages/workout/WorkoutOverviewPage.tsx
@@ -46,21 +46,21 @@ export default function WorkoutOverviewPage() {
   }, [workouts, workoutList.refetch]);
 
   return (
-      <div className="overflow-y-auto min-h-full flex flex-col p-4">
+      <div className="overflow-y-auto min-h-full flex flex-col p-4 gap-4">
         <DragDropProvider
           manager={manager}
           onDragEnd={(event) => {
             // #TODO add local backend ordering.
             setWorkouts((workout) => move(workout, event));
           }}>
-          <ul className="w-full h-full flex-1 flex flex-col gap-4">
+          <ul className="w-full flex-1 flex flex-col gap-4">
             {/*one-line if statement*/}
             {workoutList.isLoading ? <><WorkoutItemSkeleton/><WorkoutItemSkeleton/><WorkoutItemSkeleton/><WorkoutItemSkeleton/><WorkoutItemSkeleton/><WorkoutItemSkeleton/></> :null}
-            {workoutItemList.length < 0 && !workoutList.isLoading && !workoutList.isError ? <li className={"text-center"}>No workouts yet. Create a new one!</li> : workoutItemList}
+            {workoutItemList.length < 0 && !workoutList.isLoading && !workoutList.isError ? <li className={"text-center"}>No workouts yet. Create a new one!</li> : [workoutItemList,workoutItemList,workoutItemList,workoutItemList,workoutItemList]}
           </ul>
         </DragDropProvider>
 
-        <PrimaryButton>
+        <PrimaryButton className="p-4">
           <Link className={"w-full h-full text-center p-4"} to={ROUTES.WORKOUT_CREATE}>Create new workout</Link>
         </PrimaryButton>
       </div>
