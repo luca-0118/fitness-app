7fe5e16badc898d1f504beb57150b971318c29f8
Lars Konermann <144104477+larskonermann@users.noreply.github.com>
2026-05-19 12:00:46 +0200

    - Removed widgets on food page - fixed backend with front-end

--- src/pages/KcalTracker.tsx
+++ src/pages/KcalTracker.tsx
@@ -19,23 +19,12 @@ export default function KcalTracker() {
   "
           >
             <div className="grid grid-cols-2 gap-4 py-4 w-[90%] mx-auto">
-              <div className="col-span-2 bg-components border border-bordercolor rounded-xl p-6 font-bold flex flex-col items-center">
-                  <h2 className="border-b-2 border-bordercolor w-full text-center mb-4 text-lg text-textcolor">
-                      Nutrition charts
-                  </h2>
-                  <NutritionDonutChart />
-              </div>
+
+              <EatenTodayList />
+            </div>
               <div className="col-span-2">
                   <AddFoodButton to="/food-list" />
               </div>
-              <EatenTodayList />
-              <div className="bg-components border border-bordercolor rounded-xl p-6 col-span-2 items-center">
-                  <h2 className="border-b-2 border-bordercolor w-full text-center mb-4 font-bold text-lg text-textcolor">
-                      Weight
-                  </h2>
-                  <WeightLineChart />
-              </div>
-            </div>
           </div>
       </>
   );
