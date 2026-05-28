9d2ee5ab152ebd9e6f664c60c50a505e61d282f2
Zico <zicobacker26@gmail.com>
2026-05-28 11:04:31 +0200

    [Refactor] FoodItemComponent code - nutrition parsing made into a separate function - separated overlay and item from the main function

--- src/components/FoodItemComponent.tsx
+++ src/components/FoodItemComponent.tsx
@@ -11,6 +11,16 @@ interface Nutriments {
   "sodium_100g"?: number;
 }
 
+interface ParsedNutriments {
+  calories: number ;
+  carbs: number ;
+  protein: number ;
+  fat: number ;
+  sugar: number ;
+  fiber: number ;
+  sodium: number ;
+}
+
 interface FoodItemProps {
   name: string;
   nutriments: Nutriments;
@@ -19,12 +29,30 @@ interface FoodItemProps {
   onClick: () => void
 }
 
+
+/**
+ * Transforms nutrients from the database values into an object that is more easily usable.
+ * @param nutriments 
+ */
+function parseNutriments(nutriments: Nutriments): ParsedNutriments {
+  const calories = nutriments["energy-kcal_100g"] ?? 0;
+  const carbs = nutriments["carbohydrates_100g"] ?? 0
+  const protein = nutriments["proteins_100g"] ?? 0;
+  const fat = nutriments["fat_100g"] ?? 0;
+  const sugar = nutriments["sugars_100g"] ?? 0;
+  const fiber = nutriments["fiber_100g"] ?? 0;
+  const sodium = nutriments["sodium_100g"] ?? 0;
+
+  return {calories,carbs,protein,fat,sugar,fiber,sodium};
+}
+
+//note: onclick here is currently used to add it to recents.
 export default function FoodItemComponent({ name, nutriments, barcode, brand, onClick }: FoodItemProps) {
   const [overlay, setOverlay] = useState<Boolean>(false);
   const [amount, setAmount] = useState<number>(0)
   const [mealtime, setMealtime] = useState<String>("ochtend")
 
-  function handleOverlayClick() {
+  function toggleOverlay() {
     setOverlay(!overlay);
   }
 
@@ -37,23 +65,46 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
   async function addFoodToDatabase(barcode:String, date: String,name:String, amount:number, calories:number, carbs:number, fat:number, protein:number, mealtime:String){
        await invoke ("add_food", {barcode:barcode, date:date, name:name, amount:amount, calories:calories, carbs:carbs, fats:fat, protein:protein,mealtime:mealtime});
   }
+  
+  const parsedNutriments = parseNutriments(nutriments);
+
 
   function handleAddFoodClick(){
-    addFoodToDatabase(barcode, new Date().toISOString(),name, amount, Number(calories.toFixed())/100 * amount, Number(carbs.toFixed(1))/100 * amount, Number(fat.toFixed(1))/100 * amount, Number(protein.toFixed(1))/100 * amount, mealtime)
+    addFoodToDatabase(barcode, new Date().toISOString(),name, amount, Number(parsedNutriments.calories.toFixed())/100 * amount, Number(parsedNutriments.carbs.toFixed(1))/100 * amount, Number(parsedNutriments.fat.toFixed(1))/100 * amount, Number(parsedNutriments.protein.toFixed(1))/100 * amount, mealtime)
     setOverlay(false)
   }
 
 
-  const calories = nutriments["energy-kcal_100g"] ?? 0;
-  const carbs = nutriments["carbohydrates_100g"] ?? 0
-  const protein = nutriments["proteins_100g"] ?? 0;
-  const fat = nutriments["fat_100g"] ?? 0;
-  const sugar = nutriments["sugars_100g"] ?? 0;
-  const fiber = nutriments["fiber_100g"] ?? 0;
-  const sodium = nutriments["sodium_100g"] ?? 0;
+  if (overlay === true) return <Overlay 
+    name={name} 
+    nutriments={parsedNutriments} 
+    barcode={barcode} 
+    brand={brand}
+    updateAmount={(val:number) => {setAmount(val)}}
+    updateMealTime={(val:string) => {setMealtime(val)}}
+    disableOverlay={() => setOverlay(false)}
+    addProduct={handleAddFoodClick}
+    onClick={onClick} // adds item to recents.
+    />
+  if (!overlay) return <Item handleClick={toggleOverlay} name={name}/>
+}
 
-  return overlay ? (
-    <div className="z-10 bg-gray-600 fixed top-0 right-0 left-0 bottom-0 w-full h-full pt-40 mt-10 " onClick={() => onClick()}>
+
+
+
+interface OverlayProps {
+  name: string;
+  nutriments: ParsedNutriments;
+  brand: string;
+  barcode: string;
+  disableOverlay: () => void;
+  updateAmount: (val:number) => void;
+  updateMealTime: (val:string) => void;
+  addProduct: () => void;
+  onClick: () => void;
+}
+function Overlay({name,nutriments,brand,barcode, disableOverlay, updateAmount, updateMealTime, addProduct, onClick}:OverlayProps) {
+  return     <div className="z-10 bg-gray-600 fixed top-0 right-0 left-0 bottom-0 w-full h-full pt-40 mt-10 " onClick={() => onClick()}> {/** this onclick adds the fooditem to recents. */}
       <div className="fixed inset-0 top-15 bottom-15 bg-background z-20 overflow-y-auto px-5 py-5 no-scrollbar">
         <h2 className="text-textcolor text-center text-2xl font-bold mb-3">{name}</h2>
         <div className="w-full max-w-md mx-auto bg-components border border-bordercolor rounded-xl p-5">
@@ -66,7 +117,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Calories
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {calories.toFixed()}
+                {nutriments.calories.toFixed()}
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#DC143C]">
@@ -74,7 +125,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Carbs
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {carbs.toFixed(1)}g
+                {nutriments.carbs.toFixed(1)}g
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#4DA3FF]">
@@ -82,7 +133,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Proteins
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {protein.toFixed(1)}g
+                {nutriments.protein.toFixed(1)}g
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#32CD32]">
@@ -90,7 +141,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Fats
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {fat.toFixed(1)}g
+                {nutriments.fat.toFixed(1)}g
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#FFD700]">
@@ -98,7 +149,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Sugar
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {sugar.toFixed(1)}g
+                {nutriments.sugar.toFixed(1)}g
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#9153cc]">
@@ -106,7 +157,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Fiber
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {fiber.toFixed(1)}g
+                {nutriments.fiber.toFixed(1)}g
               </div>
             </div>
             <div className="border-2 rounded-xl p-5  flex flex-col items-center justify-center border-[#FF4500]">
@@ -114,7 +165,7 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
                 Sodium
               </div>
               <div className="text-textcolor inline-flex items-baseline">
-                {sodium.toFixed(1)}g
+                {nutriments.sodium.toFixed(1)}g
               </div>
             </div>
           </div>
@@ -136,9 +187,9 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
               type="text"
               placeholder="0"
               className="bg-components text-textcolor placeholder:text-gray-500 border border-bordercolor rounded-xl p-2 w-25 focus:outline-none focus:ring-2 focus:ring-accent"
-              onChange={(e)=>{setAmount(Number(e.target.value))}}
+              onChange={(e)=>updateAmount(Number(e.target.value))}
             />
-            <select onChange={(e)=> setMealtime(e.target.value)}
+            <select onChange={(e)=> updateMealTime(e.target.value)}
               defaultValue="ochtent"
               className="bg-components text-textcolor border border-bordercolor rounded-xl p-2 w-32 focus:outline-none focus:ring-2 focus:ring-accent ml-3"
             >
@@ -149,10 +200,10 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
           </div>
           <div className="w-full max-w-md mx-auto mt-5">
             <div className=" flex">
-              <button onClick={() => { setOverlay(false) }} className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-components hover:bg-components-hover active:bg-components-hover flex z-30">
+              <button onClick={() => disableOverlay() } className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-components hover:bg-components-hover active:bg-components-hover flex z-30">
                 Cancel
               </button>
-              <button className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-accent hover:bg-accent-action active:bg-accent-action flex z-30" onClick={()=>{handleAddFoodClick()}}>
+              <button className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-accent hover:bg-accent-action active:bg-accent-action flex z-30" onClick={()=>addProduct()}>
                 Add product
               </button>
             </div>
@@ -160,10 +211,17 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
         </div>
       </div>
     </div>
-  ) : (
-    <div
+}
+
+
+interface ItemProps {
+  handleClick: () => void;
+  name: string;
+}
+function Item({handleClick, name}: ItemProps) {
+  return <div
       className="bg-components border-bordercolor border rounded-xl px-2 mb-3 flex w-[90%] items-center mx-auto hover:bg-components-hover active:bg-components-hover cursor-pointer"
-      onClick={() => handleOverlayClick()}
+      onClick={() => handleClick()}
     >
       <div className="pl-3 p-6">
         <div className="flex-1 text-xl text-textcolor text-left cursor-pointer mb-auto">
@@ -171,5 +229,4 @@ export default function FoodItemComponent({ name, nutriments, barcode, brand, on
         </div>
       </div>
     </div>
-  );
 }
