3ae4a83012a7e72060d87882fd77a488e51cc3c6
Lars <lars.westenberg@student.hu.nl>
2026-06-11 09:13:25 +0200

    Small replace of onclick function for clarity

--- src/components/Foodtracker/listItems/FoodItemComponent.tsx
+++ src/components/Foodtracker/listItems/FoodItemComponent.tsx
@@ -150,15 +150,13 @@ function Overlay({ name, nutriments, brand, barcode, disableOverlay, updateAmoun
             <option value="avond">dinner</option>
           </select>
         </div>
-        <div className="w-full max-w-md mx-auto mt-5">
-          <div className=" flex">
+        <div className="w-full max-w-md mx-auto mt-5 flex gap-4">
             <button onClick={() => disableOverlay()} className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-components hover:bg-components-hover active:bg-components-hover flex z-30">
               Cancel
             </button>
-            <button className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-accent hover:bg-accent-action active:bg-accent-action flex z-30" onClick={() => addProduct()}>
+            <button onClick={() => addProduct()} className="cursor-pointer mx-auto sticky bottom-2 h-16 justify-center items-center font-bold w-[90%] rounded-full text-textcolor bg-accent hover:bg-accent-action active:bg-accent-action flex z-30">
               Add product
             </button>
-          </div>
         </div>
       </section>
     </div>
