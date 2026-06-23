6fb7fc2038724d23c8b48d508e8fcefc1ba390d7
Zico <150016766+Powerboy2015@users.noreply.github.com>
2026-06-23 14:58:33 +0200

    [FIX] lin reg history (#137)
    * [FIX] next workout prediction not available
    
    - fixed an issue where the next workout predciton would not flip
    
    * [FIX] exercise list out of bounds
    
    - fixed an issue where the elements would be under the navbar

--- src/Hooks/UsePredictNextWorkout.ts
+++ src/Hooks/UsePredictNextWorkout.ts
@@ -1,34 +1,36 @@
 import { invoke } from "@tauri-apps/api/core";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 interface GraphResponse {
-    ok: boolean,
-    data: number[]
+    ok: boolean;
+    data: number[];
 }
 
 export default function usePredictNextWorkout(exercise_id: string): number[] {
-    const [datapoints,setDataPoints] = useState<number[]>([]);
+    const [datapoints, setDataPoints] = useState<number[]>([]);
 
     const getDataPoints = async () => {
-        const response = await invoke<GraphResponse>("create_predictive_graph",{exerciseId: exercise_id});
+        const response = await invoke<GraphResponse>("create_predictive_graph", { exerciseId: exercise_id });
 
         if (response.ok) {
-            const plotPoints = response.data
-                    
-            if (plotPoints && plotPoints.find(p => p === null) != 0) {
-                console.warn("null value found. Removed it but be careful. This means a workout hasn't been filled properly.");
-                return setDataPoints(plotPoints.filter(p => p === null));
+            const plotPoints = response.data;
+
+            console.log(plotPoints);
+
+            if (plotPoints && plotPoints.findIndex((p) => p === null) !== -1) {
+                console.warn(
+                    "null value found. Removed it but be careful. This means a workout hasn't been filled properly.",
+                );
+                return setDataPoints(plotPoints.filter((p) => p !== null));
             }
 
             return setDataPoints(response.data);
         }
-
-    }
+    };
 
     useEffect(() => {
         getDataPoints();
-    },[exercise_id]);
+    }, [exercise_id]);
 
     return datapoints;
-
 }
